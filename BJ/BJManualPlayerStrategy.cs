a105725d7827293ad5a5325005aac1d2b0ea503b
Eugenijus <its_me2000@mail.ru>
2021-04-15 16:42:22 +0200

    Create BJManualPlayerStrategy.cs

---
+++ BJ/BJManualPlayerStrategy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace BJ
+{
+    public class BJManualPlayerStrategy : PlayerStrategy
+    {
+        public BJManualPlayerStrategy()
+        {
+        }
+
+        public bool IsWaitingForCard(Hand ownHand, Hand opponentHand)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
