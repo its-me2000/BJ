8ed62d105266949b3077e092c423de82490bc982
Eugenijus <its_me2000@mail.ru>
2021-04-15 17:13:54 +0200

    strategy implementation

--- BJ/BJManualPlayerStrategy.cs
+++ BJ/BJManualPlayerStrategy.cs
@@ -1,15 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace BJ
 {
     public class BJManualPlayerStrategy : PlayerStrategy
     {
+        private const uint BLACK_JACK = 21;
         public BJManualPlayerStrategy()
         {
         }
 
-        public bool IsWaitingForCard(Hand ownHand, Hand opponentHand)
+        public bool IsWaitingForCard(Hand ownHand, List<Hand> opponentsHands)
         {
-            throw new NotImplementedException();
+            if (ownHand.GetHandValue() >= BLACK_JACK) return false;
+
+            Console.WriteLine("\nMotspilleres kort:");
+            foreach (Hand hand in opponentsHands)
+            {
+                Console.WriteLine("{0} | {1}",hand.GetHandValue(),hand.ToString());
+            }
+            Console.WriteLine("\nDine kort");
+            Console.WriteLine("{0} | {1}", ownHand.GetHandValue(), ownHand.ToString());
+            Console.WriteLine("\nEtt kort til? j/n");
+            ConsoleKey commandKey = Console.ReadKey(true).Key;
+            if( commandKey == ConsoleKey.J)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
