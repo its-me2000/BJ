37127160ae5e950a36649f43f197c0f355fb2797
Eugenijus <its_me2000@mail.ru>
2021-04-15 13:45:43 +0200

    tests

--- BJ/Program.cs
+++ BJ/Program.cs
@@ -10,11 +10,35 @@ namespace BJ
 
             Console.WriteLine(new Card(CardSuit.HEARTS, CardValue.ACE).ToString());
 
-            Console.WriteLine(new NavCardDeck());
+            Console.WriteLine("\n");
+//            Console.WriteLine(new NavCardDeck());
+
+            Console.WriteLine("\n");
 
             Console.WriteLine(new DefaultCardDeck(false));
+            Console.WriteLine("\n");
 
             Console.WriteLine(new DefaultCardDeck(true));
+            Console.WriteLine("\n");
+
+            const uint threshold = 17;
+
+            new Printer(
+                new BJGame(
+                    new BJTable(
+                        new BJPlayer("Eugenijus", new BJThresholdPlayerStategy(threshold)),
+                        new BJPlayer("NAV", new BJDealerPlayerStrategy())
+                    ),
+                    new NavCardDeck(),
+                    new BJRules()
+                )
+            );
+
+            //Hand hand = new BJHand();
+            //hand.TakeCard(new Card(CardSuit.CLUBS, CardValue.ACE));
+            //hand.TakeCard(new Card(CardSuit.DIAMONDS, CardValue.EIGHT));
+            //Console.WriteLine(hand.GetHandValue());
+
             Console.ReadKey(true);
         }
     }
