f48d13cbaf73755e84c352dc851c62e20f161fa8
Eugenijus <its_me2000@mail.ru>
2021-04-15 10:49:22 +0200

    Fetching data from Nav

--- BJ/NavCardDeck.cs
+++ BJ/NavCardDeck.cs
@@ -1,24 +1,112 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace BJ
 {
     public class NavCardDeck : CardDeck
     {
         private readonly Queue<Card> deck;
+        private const int CARDS_IN_DECK = 52;
+        static private readonly HttpClient client = new HttpClient();
+        private static readonly Dictionary<string, CardValue> StringToCardValue = new Dictionary<string, CardValue>()
+        {
+            { "2", CardValue.TWO }, { "3", CardValue.THREE }, { "4", CardValue.FOUR  }, { "5", CardValue.FIVE },
+            { "6", CardValue.SIX }, { "7", CardValue.SEVEN }, { "8", CardValue.EIGHT }, { "9", CardValue.NINE },
+            { "10", CardValue.TEN}, { "J", CardValue.JACK  }, { "Q", CardValue.QUEEN }, { "K", CardValue.KING },
+            { "A", CardValue.ACE }
+        };
+        private static readonly Dictionary<string, CardSuit> StringToCardSuit = new Dictionary<string, CardSuit>()
+        {
+            { "CLUBS",    CardSuit.CLUBS    },
+            { "DIAMONDS", CardSuit.DIAMONDS },
+            { "HEARTS",   CardSuit.HEARTS   },
+            { "SPADES",   CardSuit.SPADES   }
+        };
 
-        public NavCardDeck()
+        private class JsonObject
         {
-            private class JsonObject
+            public string suit;
+            public string value;
+        }
+
+        
+        public NavCardDeck(string url)
         {
+            try
+            {
+                deck = JsonObjectListToQueue(DeserializeJsonObjectList(GetJsonString(url)));
+                if(deck.Count != CARDS_IN_DECK)
+                {
+                    throw new Exception("Bad Deck.");
+                }
+            }
+            catch( Exception e )
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+                Console.ReadKey(true);
+                throw e;
+            }
 
         }
+
+        private static string GetJsonString(string url)
+        {
+            try
+            {
+                return client.GetStringAsync(url).Result;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+                Console.ReadKey(true);
+                throw e;
+            }
+        }
+        private static List<JsonObject> DeserializeJsonObjectList(string jsonString)
+        {
+            try
+            {
+                return JsonSerializer.Deserialize<List<JsonObject>>(jsonString);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+                Console.ReadKey(true);
+                throw e;
+            }
+        }
+        private static Queue<Card> JsonObjectListToQueue(List<JsonObject> jsonList)
+        {
+            try
+            {
+
+                Queue<Card> queue = new Queue<Card>();
+                foreach (JsonObject obj in jsonList)
+                {
+                    queue.Enqueue(new Card(StringToCardSuit[obj.suit], StringToCardValue[obj.value]));
+                }
+                return queue;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+                Console.ReadKey(true);
+                throw e;
+            }
         }
 
         public Card GetCard()
         {
-            //return deck.Dequeue();
-            return new Card(CardSuit.HEARTS, CardValue.ACE);
+            return deck.Dequeue();
         }
+
         public override string ToString()
         {
             string returnString = "";
