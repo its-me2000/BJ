b539160e890163dfae58abb2d02457b9c29d1829
Eugenijus <its_me2000@mail.ru>
2021-04-16 09:32:24 +0200

    fixes and refactoring

--- BJ/BJDealerPlayerStrategy.cs
+++ BJ/BJDealerPlayerStrategy.cs
@@ -1,24 +1,48 @@
-﻿using System.Collections.Generic;
+﻿/*
+ * ....begynner Marit å trekke kort
+ * Marit slutter å trekke kort når poengsummen hennes er høyere enn din poengsum
+ * Marit taper spillet dersom poengsummen er høyere enn 21
+ * 
+ * Remark:
+ * Om dine poengsum er 20 eller 21 og Marits poengsum er 20 eller 21
+ * det er ingen vits å trekke en kort til for henne.
+ */
+
+using System.Collections.Generic;
 
 namespace BJ
 {
     public class BJDealerPlayerStrategy : PlayerStrategy
     {
         private const uint BLACK_JACK = 21;
+
         public BJDealerPlayerStrategy()
         {
         }
 
-        public bool IsWaitingForCard(Hand ownHand, List<Hand> opponentsHands)
+        public bool IsWaitingForCard(Player selfPlayer, Table table)
         {
             uint bestOpponentsHandValue = 0;
-            opponentsHands.ForEach( (hand) => {
-                if (hand.GetHandValue() > bestOpponentsHandValue && hand.GetHandValue() <= BLACK_JACK)
-                    bestOpponentsHandValue = hand.GetHandValue();
+            table.GetPlayers().ForEach( (player) => {
+            if (player != selfPlayer
+                && player.GetHand().GetHandValue() > bestOpponentsHandValue
+                && player.GetHand().GetHandValue() <= BLACK_JACK)
+                {
+                    bestOpponentsHandValue = player.GetHand().GetHandValue();
+                }
             });
 
-            return ownHand.GetHandValue() <= bestOpponentsHandValue
-                && ownHand.GetHandValue() < BLACK_JACK;
+            /*
+             * Marit slutter å trekke kort når poengsummen hennes er høyere enn din poengsum
+             */
+            //return selfPlayer.GetHand().GetHandValue() <= bestOpponentsHandValue;
+
+            /*
+             * Marit slutter å trekke kort når hun vinner eller mistet mulighet til å vinne.
+             */
+            bool ownValueLessOrEqThanOpponents = selfPlayer.GetHand().GetHandValue() <= bestOpponentsHandValue;
+            bool ownValueLessThanTwenty = selfPlayer.GetHand().GetHandValue() < BLACK_JACK - 1;
+            return ownValueLessOrEqThanOpponents && ownValueLessThanTwenty;          
         }
     }
 }
