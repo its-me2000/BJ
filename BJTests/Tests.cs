719a06d1052a9e6f217d21a383180b17a888abde
Eugenijus <its_me2000@mail.ru>
2021-04-16 14:15:52 +0200

    Added test
    some simple Card and BJHand tests

--- BJTests/Tests.cs
+++ BJTests/Tests.cs
@@ -12,5 +12,19 @@ namespace BJTests
             Card card = new Card(CardSuit.HEARTS, CardValue.ACE);
             Assert.AreEqual("HA", card.ToString(),"Bad card constructed.");
         }
+
+        [TestMethod]
+        public void TestBJHand()
+        {
+            Hand hand = new BJHand();
+            Card card = new Card(CardSuit.DIAMONDS, CardValue.FIVE);
+            hand.TakeCard(card);
+            card = new Card(CardSuit.SPADES, CardValue.KING);
+            hand.TakeCard(card);
+            card = new Card(CardSuit.CLUBS, CardValue.TEN);
+            hand.TakeCard(card);
+            Assert.AreEqual(25u, hand.GetHandValue(), "Wrong value calculation.");
+            Assert.ThrowsException<System.NotImplementedException>(() => _ = hand.ThrowCard(0));
+        }
     }
 }
