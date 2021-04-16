1ab2d954ef8d542603d855c8cbfd00c2d8a18c3a
Eugenijus <its_me2000@mail.ru>
2021-04-16 09:39:10 +0200

    refactoring

--- BJ/Printer.cs
+++ BJ/Printer.cs
@@ -1,33 +1,53 @@
-﻿using System;
+﻿/*
+ * Skriv ut hvem som vinner spillet
+ * Skriv ut poengsum og kortene til hver spiller ved spillets slutt.
+ * Et kort angis ved å konkatenere suit og value:
+ * suit -> [H|D|S|C]
+ * value -> [2|3|4|5|6|7|8|9|10|J|Q|K|A]
+ * Eksempel på output:
+ *
+ *
+ * Vinner: Truls
+ *
+ * Marit | 27 | S7,S10,CJ
+ * Truls  | 19 | D2,H2,C6,H9
+ * 
+ */
+
+using System;
 using System.Collections.Generic;
 
 namespace BJ
 {
     public class Printer
     {
-        public Printer(Game game)
+        Game game;
+        public Printer(Game _game)
         {
-            Console.WriteLine("!!!BLACK JACK!!!");
+            game = _game;
+        }
+        public void Print()
+        {
             List<Player> Winners = game.GetWinners();
 
             string winnersString;
             if (Winners.Count == 0)
             {
-                winnersString = "Ingen vinner.";
+                winnersString = "\nIngen vinner.";
             }
             else
             {
                 winnersString = Winners[0].GetName();
                 if(Winners.Count == 1)
                 {
-                    winnersString = "Vinner: " + winnersString;
+                    winnersString = "\nVinner: " + winnersString;
                 }
                 else
                 {
-                    winnersString = "Vinnere: " + winnersString;
+                    winnersString = "\nVinnere: " + winnersString;
                     foreach(Player player in Winners.GetRange(1, Winners.Count - 1))
                     {
-                        winnersString += player.GetName();
+                        winnersString += ", "+player.GetName();
                     }
                 }
             }
@@ -38,5 +58,6 @@ namespace BJ
                 Console.WriteLine(player.ToString());
             }
         }
+
     }
 }
