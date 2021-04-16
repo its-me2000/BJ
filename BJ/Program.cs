b8927aafe9f327a9b9984ab113167cd3b90d56f8
Eugenijus <its_me2000@mail.ru>
2021-04-16 09:39:44 +0200

    Added User Interface

--- BJ/Program.cs
+++ BJ/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BJ
 {
@@ -6,40 +7,147 @@ namespace BJ
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ConsoleKey commandKey;
+            Console.WriteLine("\nVelkomment til BLACK JACK spill!");
+            do {
+                Console.WriteLine("\n (1) - Kjør demo. (Enter) - Spill. (Esc) - Slutt.");
+                commandKey = Console.ReadKey(true).Key;
 
-            Console.WriteLine(new Card(CardSuit.HEARTS, CardValue.ACE).ToString());
+                switch (commandKey)
+                {
+                    case ConsoleKey.D1:
+                        RunDemo();
+                        break;
+                    case ConsoleKey.Enter:
+                        RunGame();
+                        break;
+                    default:
+                        break;
+                       
+                }
+            } while (commandKey != ConsoleKey.Escape) ;
+            Console.WriteLine("\nBye-Bye!");
+            Console.ReadKey(true);
+        }
 
-            Console.WriteLine("\n");
-//            Console.WriteLine(new NavCardDeck());
-
-            Console.WriteLine("\n");
-
-            Console.WriteLine(new DefaultCardDeck(false));
-            Console.WriteLine("\n");
-
-            Console.WriteLine(new DefaultCardDeck(true));
-            Console.WriteLine("\n");
-
-            const uint threshold = 17;
-
-            new Printer(
+        private static void RunDemo()
+        {
+            string demoPlayerName = "Truls";
+            string demoDealerName = "Marit";
+            Printer GameResultsPrinter = new Printer(
                 new BJGame(
                     new BJTable(
-                        new BJPlayer("Eugenijus", new BJThresholdPlayerStategy(threshold)),
-                        new BJPlayer("NAV", new BJDealerPlayerStrategy())
+                        new BJPlayer(demoPlayerName, new BJThresholdPlayerStrategy()),
+                        new BJPlayer(demoDealerName, new BJDealerPlayerStrategy())
                     ),
                     new NavCardDeck(),
                     new BJRules()
                 )
             );
+            GameResultsPrinter.Print();
+        }
 
-            //Hand hand = new BJHand();
-            //hand.TakeCard(new Card(CardSuit.CLUBS, CardValue.ACE));
-            //hand.TakeCard(new Card(CardSuit.DIAMONDS, CardValue.EIGHT));
-            //Console.WriteLine(hand.GetHandValue());
+        private static void RunGame()
+        {
+            uint numberOfPlayers;
+            Console.WriteLine("\nHvor mange spillere? ( standard: 1 ) :");
 
-            Console.ReadKey(true);
+            string inputString = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(inputString))
+            {
+                //Default Value
+                numberOfPlayers = 1;
+            }
+            else if (!uint.TryParse(inputString, out numberOfPlayers) || numberOfPlayers < 1)
+            {
+                Console.WriteLine("\nFeil verdi.");
+                Console.ReadKey(true);
+                return;
+            }
+
+            List<Player> players = new List<Player>();
+            for(int i = 0; i < numberOfPlayers; i++)
+            {
+                Console.WriteLine("Skriv navn til Spiller nr {0} eller la tomt og trykk (Enter) for KI:",i+1);
+                inputString = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(inputString))
+                {
+                    players.Add(new BJPlayer("KI" + (i + 1).ToString(), new BJThresholdPlayerStrategy()));
+                }
+                else
+                {
+                    players.Add(new BJPlayer(inputString, new BJManualPlayerStrategy(PlayerUserInterface)));
+                }
+            }
+
+            Table table = new BJTable(players, new BJPlayer("Dealer", new BJDealerPlayerStrategy()));
+            CardDeck deck;
+            NewDeck:
+            Console.WriteLine("Velg kortstokken:\n(Enter) - NAVIKT. (Space) - Lokal.");
+            ConsoleKey commandKey = Console.ReadKey(true).Key;
+            if(commandKey == ConsoleKey.Enter)
+            {
+                try
+                {
+                    deck = new NavCardDeck();
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("\nDet oppstått en feil med lasting kortstokken fra NAVIKT.");
+
+                    Console.WriteLine("\nException Caught!");
+                    Console.WriteLine("Message :{0} ", e.Message);
+
+                    Console.WriteLine("\nLokal kortstokken brukes.");
+                    Console.ReadKey(true);
+                    deck = new DefaultCardDeck(true);
+               }
+            }
+            else
+            {
+                deck = new DefaultCardDeck(true);
+            }
+            
+            Printer GameResultsPrinter = new Printer(new BJGame(table, deck, new BJRules()));
+            NextGame:
+            GameResultsPrinter.Print();
+            Console.WriteLine("En gang til?\n (Enter) - Ja. (Space) - Ja med ny kortstokken. (Esc) - Nei.");
+            commandKey = Console.ReadKey(true).Key;
+            switch (commandKey)
+            {
+                case ConsoleKey.Enter:
+                case ConsoleKey.J:
+                    goto NextGame;
+                case ConsoleKey.Spacebar:
+                    goto NewDeck;
+                case ConsoleKey.Escape:
+                case ConsoleKey.N:
+                default:
+                    break;
+            }
+        }
+
+        private static bool PlayerUserInterface(string ownData, string opponentsData)
+        {
+            Console.Clear();
+            Console.WriteLine("\nMotspilleres kort:");
+            Console.WriteLine(opponentsData);
+            Console.WriteLine("\nDine kort:");
+            Console.WriteLine(ownData);
+
+            Console.WriteLine("\nEtt kort til? \n(Enter) - Ja. (Esc) - Nei.");
+            ConsoleKey commandKey = Console.ReadKey(true).Key;
+            switch (commandKey)
+            {
+                case ConsoleKey.J:
+                case ConsoleKey.Y:
+                case ConsoleKey.Enter:
+                case ConsoleKey.Spacebar:
+                    return true;
+                default:
+                    break;
+            }
+            return false;
         }
     }
 
